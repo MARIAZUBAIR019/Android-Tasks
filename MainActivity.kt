0e3f7f9145cb22d1a031994de5962a988a2d2f65
MARIAZUBAIR019 <80569578+MARIAZUBAIR019@users.noreply.github.com>
2021-11-08 06:01:20 -0800

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.sharereferen
+
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var sharedPreferences: SharedPreferences
+    var isRemembered = false
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        sharedPreferences = getSharedPreferences("SHARED_REF",Context.MODE_PRIVATE)
+        isRemembered = sharedPreferences.getBoolean("CHECKBOX",false)
+
+        if(isRemembered){
+            val intent = Intent(this,anther::class.java)
+            startActivity(intent)
+            finish()
+        }
+        login.setOnClickListener{
+            val name:String=name.text.toString()
+            val age:Int=age.text.toString().toInt()
+            val check:Boolean=check.isChecked
+            val prefs=getSharedPreferences("SHARED_REF", MODE_PRIVATE)
+            val editor =prefs.edit()
+            editor.putString("Name",name)
+            editor.putInt("Age",age)
+            editor.putBoolean("check",check)
+                editor.apply()
+            Toast.makeText(this,"information safe", Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this,anther::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+    }
+}
